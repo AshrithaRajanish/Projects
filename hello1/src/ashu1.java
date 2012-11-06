3fccab0655a79a38f58c1c4154a53afe5a847436
rajanish <rajanish_gj@yahoo.com>
2012-11-06 14:05:27 -0800

    showing how to change and commit

--- hello1/src/ashu1.java
+++ hello1/src/ashu1.java
@@ -1,4 +1,3 @@
-
 public class ashu1 {
 
 	/**
@@ -6,7 +5,7 @@ public class ashu1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("test");
 	}
 
 }
